f08bc9d15be80d7ee351fc7344b5d8b0f8e08861
Paul Janssens <pjs@foreach.be>
2011-08-24 12:22:28 +0000

    echte test.

--- common-web/src/test/java/com/foreach/web/mail/TestExceptionToMailResolver.java
+++ common-web/src/test/java/com/foreach/web/mail/TestExceptionToMailResolver.java
@@ -8,7 +8,10 @@ import org.junit.Test;
 import org.mockito.Matchers;
 import org.springframework.mock.web.MockHttpServletRequest;
 import org.springframework.mock.web.MockHttpServletResponse;
+import org.springframework.mock.web.MockHttpSession;
 
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpSession;
 import java.io.File;
 import java.util.Date;
 import java.util.Map;
@@ -69,7 +72,36 @@ public class TestExceptionToMailResolver
 		when( mailService.sendMimeMail( anyString(), anyString(), anyString(), anyString(), anyString(),
 		                                    Matchers.<Map<String,File>> anyObject() ) ).thenThrow( new NullPointerException() );
 
+		resolver.doResolveException( request, response, null, new Exception() );
+	}
+
+	@Test
+	public void addCookiesAndAttributesAndStuffLikeForARealMessage()
+	{
+		MockHttpServletRequest request = new MockHttpServletRequest();
+		MockHttpServletResponse response= new MockHttpServletResponse();
+
+		Cookie lu = new Cookie( "lu", "bah" );
+		Cookie delacre = new Cookie( "delacre", "gaatwel" );
+		Cookie destrooper = new Cookie( "destrooper", "toch wat vettig" );
+
+		request.setCookies( lu, delacre, destrooper  );
+
+		request.setAttribute( "casting cost", "2 witte bollekes" );
+
+		request.addParameter( "kleur", "donkere oker" );
+
+		request.addHeader( "X-Dontlookatthecontent", "if so inclined" );
+
+		HttpSession session = new MockHttpSession();
+		session.setAttribute( "sessionId", "supposedToBeALongString" );
+
+		request.setSession( session );
+
 		resolver.doResolveException( request, response, null, new Exception( ) );
+
+		verify( mailService ).sendMimeMail( eq( fromAddress ), eq( toAddress ), anyString(), anyString(), anyString(),
+		                                    Matchers.<Map<String,File>> anyObject() );
 	}
 
 }
