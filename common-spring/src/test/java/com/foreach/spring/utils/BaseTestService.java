9a34d35346bd81ed55d1f8ca2d0e4a2c1b356f24
Paul Janssens <pjs@foreach.be>
2011-08-26 09:59:30 +0000

    zo dan

---
+++ common-spring/src/test/java/com/foreach/spring/utils/BaseTestService.java
@@ -0,0 +1,12 @@
+package com.foreach.spring.utils;
+
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = { "classpath:config/spring-test-services.xml" })
+public abstract class BaseTestService
+{
+}
