84fcf7f0ae2cc6fcf1f1cf8bce8b81ab9ac6d017
Paul Janssens <pjs@foreach.be>
2011-09-02 08:09:26 +0000

    delete old classes

--- common-web/src/main/java/com/foreach/web/convertor/RecursiveConverter.java
+++
@@ -1,8 +0,0 @@
-package com.foreach.web.convertor;
-
-import org.springframework.core.convert.ConversionService;
-
-public interface RecursiveConverter
-{
-	void setConversionService( ConversionService conversionService );
-}
