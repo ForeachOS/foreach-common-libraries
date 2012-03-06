fe5902b288724a2a536aa802ba33c52409c55e7c
Arne Vandamme <arne@foreach.be>
2012-03-06 13:07:21 +0000

    0.4-SNAPSHOT: MailStatus javadoc

--- common-spring/src/main/java/com/foreach/spring/mail/MailStatus.java
+++ common-spring/src/main/java/com/foreach/spring/mail/MailStatus.java
@@ -1,5 +1,8 @@
 package com.foreach.spring.mail;
 
+/**
+ * Status object to check if mail has been sent.
+ */
 public final class MailStatus
 {
 	private final boolean mailSent;
@@ -16,11 +19,19 @@ public final class MailStatus
 		this.exception = exception;
 	}
 
+	/**
+	 * If false then the exception causing mail to fail can be found
+	 * using getException().  If any exception occurred.
+	 * @return True if mail sent ok, false otherwise.
+	 */
 	public boolean isMailSent()
 	{
 		return mailSent;
 	}
 
+	/**
+	 * @return Exception if one has been thrown.
+	 */
 	public Throwable getException()
 	{
 		return exception;
