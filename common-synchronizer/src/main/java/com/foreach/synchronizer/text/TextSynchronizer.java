42f2b37e91556071da9c65e6ce4e1bd908039190
Kjell Segers <kjs@foreach.be>
2013-08-08 16:07:16 +0200

    Added component annotation to TextSynchronizer

--- common-synchronizer/src/main/java/com/foreach/synchronizer/text/TextSynchronizer.java
+++ common-synchronizer/src/main/java/com/foreach/synchronizer/text/TextSynchronizer.java
@@ -7,7 +7,9 @@ import org.apache.commons.cli.ParseException;
 import org.apache.commons.cli.PosixParser;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
+@Component
 public class TextSynchronizer {
 
 	@Autowired
