4c20ec3a25a83386c33ccbb99d1950855fac24d4
Koen Deschacht <kds@foreach.be>
2013-08-12 17:07:18 +0200

    common-synchronizer: Configured aliases for XStream writer

--- common-synchronizer/src/main/java/com/foreach/synchronizer/text/io/XmlLocalizedTextWriter.java
+++ common-synchronizer/src/main/java/com/foreach/synchronizer/text/io/XmlLocalizedTextWriter.java
@@ -1,6 +1,10 @@
 package com.foreach.synchronizer.text.io;
 
+import com.foreach.spring.localization.AbstractLocalizedFieldsObject;
+import com.foreach.spring.localization.Language;
+import com.foreach.spring.localization.LanguageConfigurator;
 import com.foreach.spring.localization.text.LocalizedText;
+import com.foreach.spring.localization.text.LocalizedTextFields;
 import com.thoughtworks.xstream.XStream;
 import org.apache.commons.io.IOUtils;
 
@@ -10,24 +14,29 @@ import java.util.Collection;
 
 public class XmlLocalizedTextWriter implements LocalizedTextWriter {
 
-	private XStream xStream;
-	private final OutputStream outputStream;
+    private XStream xStream;
+    private final OutputStream outputStream;
 
-	public XmlLocalizedTextWriter( OutputStream outputStream ) {
-		this.outputStream = outputStream;
-		initializeXStream();
-	}
+    public XmlLocalizedTextWriter(OutputStream outputStream) {
+        this.outputStream = outputStream;
+        initializeXStream();
+    }
 
-	private void initializeXStream() {
-		xStream = new XStream();
-		//TODO configure aliases and stuff
-	}
+    private void initializeXStream() {
+        xStream = new XStream();
+        xStream.alias("text", LocalizedText.class);
+        xStream.alias("fields", LocalizedTextFields.class);
+        xStream.addDefaultImplementation(LanguageConfigurator.getLanguages()[0].getClass(), Language.class);
+        xStream.omitField(AbstractLocalizedFieldsObject.class, "fieldsAsUnmodifiableMap");
+        xStream.omitField(AbstractLocalizedFieldsObject.class, "fieldsAsModifiableCollection");
+        xStream.addImplicitMap(LocalizedText.class, "fieldsByLanguageCode", LocalizedTextFields.class, "language");
+    }
 
-	public void write( Collection<LocalizedText> localizedTexts ) {
-		xStream.toXML( localizedTexts, outputStream );
-	}
+    public void write(Collection<LocalizedText> localizedTexts) {
+        xStream.toXML(localizedTexts, outputStream);
+    }
 
-	public void close() throws IOException {
-		IOUtils.closeQuietly( outputStream );
-	}
+    public void close() throws IOException {
+        IOUtils.closeQuietly(outputStream);
+    }
 }
